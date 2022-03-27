adff435efe5631ef24a7a3bfe43498fd5d83d66f
westnine <seokwoo2000@naver.com>
2022-03-27 23:16:58 +0900

    예제 워드 리스트 작성완료 [---] 저장 방식 > 업데이트 방식으로 편경 endl, \n 정리 완료 영어패치 완료
    [To do]
    randint 고정값 해결 필요

--- main.cpp
+++ main.cpp
@@ -2,76 +2,87 @@
 
 string swapping(int count, string answer) {
 	while (count--) {
-		int l = answer.length();
+		int l = (int)answer.length();
 		swap(answer[randint(l - 1)], answer[randint(l - 1)]);
+		cout << randint(l - 1) << "<->" << randint(l - 1) << endl;
 	}
 	return answer;
 }
 void solvingPuzzle(int chance, string answer) {
 	int count = 1;
 	bool success = false;
-	int length = answer.length();
+	int answerLength = (int)answer.length();
 	string userAnswer = "";
-	string checkWithAnswer = "[";
-	checkWithAnswer.append(length,  '-');
-	checkWithAnswer.append("]");
+	
 	
 	while (count <= chance) {
-		cout << "[" << count++ << "/" << chance << "] 맞춰봐 : "; //영어로변환
+		cout << "[" << count++ << "/" << chance << "] Guess the original word: ";
 		cin >> userAnswer;
+		int userLength = (int)userAnswer.length();
 
-		for (int i = 0; i < length; i++) 
+		string checkWithAnswer = "";
+		checkWithAnswer.append(answerLength, '-');
+	
+		for (int i = 0; i < min(answerLength,userLength); i++)
 			if (userAnswer[i] == answer[i])
-				checkWithAnswer[i + 1] = answer[i];
+				checkWithAnswer[i] = answer[i];
 
-		cout << ">>" << checkWithAnswer << endl << endl;
+		cout << ">> [" << checkWithAnswer << "]" << endl << endl;
+		
+
 		if (userAnswer == answer) {
 			success = true;
 			break; 
 		}
 	}
 	if (success)
-		cout << "당신은 맞았어" << endl;
-	else
-		cout << "당신은 틀렸어" << endl;
+		cout << "Congratulation! You got the answer right!" << endl;
+	else {
+		cout << "Oops! You failed to solve the puzzle..." << endl;
+		cout << "The correct answer is '" << answer << "'." << endl;
+	}
+	cout << "\n";
 }
-void solvedPuzzle(vector<string> wordList) {
+void solvePuzzle(vector<string> wordList) {
 	int opt = 0;
 	int chance = 0;
 	string problem = "";
 	string answer = "";
 	while (opt != 3) {
-		cout << endl << "[  Solved Puzzle  ]" << endl;
-		cout << "1. Easy mode\n" << "2. Hard Mode\n" << "3. Return\n" << endl;
-		while (1) {
+		cout  << "[ SOLVE PUZZLE ]" << endl;
+		cout << "1. Easy mode" << endl;
+		cout << "2. Hard Mode" << endl;
+		cout << "3. Return" << endl;
+		cout << "\n";
+
+		while (true) {
 			cout << "Choose an option (1-3): ";
 			cin >> opt;
-			if (opt == 1 || opt == 2 || opt == 3) break;
+			if (opt == 1 || opt == 2 || opt == 3) 
+				break;
 		}
+		cout << endl;
+		
 		switch (opt) {
 		case 1: //Easy Mode
-		{
 			chance = 3;
-			do answer = wordList[randint(wordList.size() - 1)];
+			do answer = wordList[randint((int)wordList.size() - 1)];
 			while (answer.length() > 5);  
 			problem = swapping(3, answer);
-			cout << endl << "문제 : \'" << problem << "\'." << endl;
+			cout << "I give you a jumbled word : \'" << problem << "\'." << endl;
 			solvingPuzzle(3, answer);
 			break;
-		}
 		case 2: //Hard Mode
-		{
 			chance = 5;
-			do answer = wordList[randint(wordList.size() - 1)];     //위랑 구조 비슷하니까 wordlist를 클래스로 선언하고 그걸 참조하는 형식으로 만들서 하나의 함수로 합치자
+			do answer = wordList[randint((int)wordList.size() - 1)];     
 			while (answer.length() < 5);
 			problem = swapping(4, answer);
-			cout << endl << "문제 : \'" << problem << "\'." << endl;
-			solvingPuzzle(3, answer);
+			cout << "I give you a jumbled word : \'" << problem << "\'." << endl;
+			solvingPuzzle(5, answer);
 			break;
-		}
 		case 3: break; //Return 
-		default: break;
 		}
+		
 	}
 	return;
 }
@@ -79,54 +90,70 @@ vector<string> manageList(vector<string> wordList) {
 	int opt = 0;
 	while (opt != 3) {
 		string userInputWord = "";
-		auto it = wordList.begin();
-		cout << endl << "[  Manage List  ]" << endl;
-		cout << "1. Print Words\n" << "2. Add Word\n" << "3. Return\n" << endl;
-		while (1) {
+		cout << "[ MANAGE LIST ]" << endl;
+		cout << "1. Print Words" << endl;
+		cout << "2. Add Word" << endl;
+		cout << "3. Return" << endl;
+		cout << "\n";
+		while (true) {
 			cout << "Choose an option (1-3): ";
 			cin >> opt;
 			if (opt == 1 || opt == 2 || opt == 3) break;
 		}
+		cout << "\n";
+
 		switch (opt) {
 		case 1: //PrintWord
-			cout << endl << "리스트에 단어가" << wordList.size() << "종류 있습니다." << endl;
-			for (string str : wordList) cout << str << endl;  break;
+			cout << "A total of " << wordList.size() << " words are available:" << endl;
+			for (string str : wordList)
+				cout << str << endl;
+			cout << "\n";
+			break;
 		case 2: //Add Word
-			cout << endl << "추가할 단어를 입력하시오: ";
+		{
+			cout << "Input a new word: ";
 			cin >> userInputWord;
-			it = find(wordList.begin(), wordList.end(), userInputWord);
-			if (it != wordList.end()) cout << endl <<"해당 단어가 이미 존재합니다" << endl;
+
+			auto it = find(wordList.begin(), wordList.end(), userInputWord);
+
+			if (it != wordList.end())
+				cout << "The word \"" << userInputWord << "\" already exists in the list." << endl;
 			else {
 				wordList.push_back(userInputWord);
-				cout << "\"" << userInputWord << "\"가 성공적으로 리스트에 추가 되었습니다." << endl;
+				cout << "The word \"" << userInputWord << "\" has been sucessfully inserted in the list." << endl;
 			}
+			cout << "\n";
 			break;
+		}
 		case 3: //Return
 			break;
-		default: break;
 		}
+
 		sort(wordList);
 	}
 	return wordList;
 }
-
 int main() {
-	vector<string> wordList = { "ipad","apple","light","helicopter" }; 	
+	vector<string> wordList = { "drift", "professional","midnight","scatter","relevance","visit","disaster","commerce","coincide","progress","shift","spell","veil","desk","gown","umbrella","gregarious","craftman","wriggle","score","deliver","vote","dirty","chain","road","charter","feign","duck","highway","add","van","mutual","bang","table","venture","deficiency","depend","dimension","holiday","scrap","unlike","coast","command","base","recommand","spit","corner","huge","economic","sigh" };
 	sort(wordList);
 
 	int opt = 0;
 	while (opt != 3){
-		cout << endl << "<<<  WORD JUMBLE  >>>" << endl;
-		cout << "1. Manage List" << endl << "2. Solved Puzzle" << endl << "3. Exit" << endl;
+		cout << "<<<  WORD JUMBLE  >>>" << endl;
+		cout << "1. Manage List" << endl;
+		cout << "2. Solve Puzzle" << endl;
+		cout << "3. Exit" << endl;
+		cout << "\n";
 		while (1) {
 			cout << "Choose an option (1-3): ";
 			cin >> opt;
 			if (opt == 1 || opt == 2 || opt == 3) break;
 		}
+		cout << endl;
 		switch (opt) {
 		case 1: wordList = manageList(wordList); break;
-		case 2: solvedPuzzle(wordList); break;
-		case 3: cout << "Program Exit" << endl; break;
+		case 2: solvePuzzle(wordList); break;
+		case 3: cout << "Good Bye~" << endl; break;
 		default: break;
 		}
 	}
